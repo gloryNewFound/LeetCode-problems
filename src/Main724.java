57788070e4981fe38f29d5d2305f8fca51d29829
dvn <glorynewfound@yandex.ru>
2022-10-16 00:22:08 +0300

    724. Find Pivot Index best solution

---
+++ src/Main724.java
@@ -0,0 +1,27 @@
+public class Main724 {
+
+    public static void main(String[] args) {
+        int[] nums = {2,1,-1};
+        System.out.println((new Main724()).pivotIndex(nums));
+
+    }
+
+    public int pivotIndex(int[] nums) {
+        int sumLeft = 0;
+        int sumRight = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sumRight += nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            sumRight -= nums[i];
+            if (sumLeft == sumRight) {
+                return i;
+            } else {
+                sumLeft += nums[i];
+
+            }
+        }
+
+        return -1;
+    }
+}
