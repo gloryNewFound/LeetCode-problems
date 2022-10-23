4c20b06e30865535364dde1a14d6405e27493f3d
dvn <glorynewfound@yandex.ru>
2022-10-23 13:35:56 +0300

    Commit

--- src/Solution409.java
+++ src/Solution409.java
@@ -1,2 +1,12 @@
 public class Solution409 {
+    public static void main(String[] args) {
+        String s = "dccaccd";
+        System.out.println(longestPalindrome(s));
+    }
+
+    public static int longestPalindrome(String s) {
+        //решить заново
+
+        return 0;
+    }
 }
